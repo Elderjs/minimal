8b7c8e98c13c4b0f4ad475572bc2649c00fdeff1
Meigo Kukk <meigo@animal.ee>
2020-10-05 19:23:38 +0300

    Add SEO head elements for 4x100% lighthouse score.

--- src/routes/simple/Simple.svelte
+++ src/routes/simple/Simple.svelte
@@ -2,6 +2,9 @@
   export let data, helpers,  request, settings;
 </script>
 
-
+<svelte:head>
+  <title>Simple | Elderjs minimal</title>
+  <meta name="description" content="Put your description here." />
+</svelte:head>
 
 Simple route.
